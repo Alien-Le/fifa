9255c9b1fc459e8116171f2f36dc69505633108b
Lesha <xmrerx@gmail.com>
2019-11-18 19:01:21 +0200

    move the data to  mysql database

---
+++ fill_database.py
@@ -0,0 +1,86 @@
+import sys
+import pandas as pd
+import pymysql
+from pymysql.cursors import DictCursor
+from contextlib import closing
+
+config_path = '/srv/alien'
+sys.path.append(config_path)
+
+import config
+
+
+dbname = config.MYSQL['db']
+config.MYSQL['db']  = ''
+with closing(pymysql.connect(**config.MYSQL)) as connection:
+    with connection.cursor(DictCursor) as cursor:
+        cursor.execute('CREATE DATABASE IF NOT EXISTS ' + dbname)
+config.MYSQL['db']  = dbname
+
+
+players = pd.read_csv('data.csv')
+nationalities = list(players['Nationality'].unique())
+clubs = [club for club in list(players['Club'].unique()) if str(club) != 'nan']
+
+group = players.sort_values(['Age', 'Unnamed: 0'], ascending=[True, True]).groupby('Age')
+players['position_in_sprite'] = group.cumcount()
+
+with closing(pymysql.connect(**config.MYSQL)) as connection:
+    with connection.cursor(DictCursor) as cursor:
+        cursor.execute('''
+        CREATE TABLE IF NOT EXISTS nationality (
+            nationality_id INT(11) NOT NULL AUTO_INCREMENT,
+            nationality VARCHAR(255) NOT NULL,
+            PRIMARY KEY (nationality_id),
+            UNIQUE KEY (nationality)
+        )  ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci''')
+        cursor.execute('INSERT IGNORE INTO nationality (nationality) VALUES ' + ', '.join(['(%s)'] * len(nationalities)), nationalities)
+        cursor.execute('SELECT * FROM nationality')
+        db_nationalities = {}
+        for db_nationality in cursor.fetchall():
+            db_nationalities[db_nationality['nationality']] = db_nationality['nationality_id']
+        cursor.execute('''
+        CREATE TABLE IF NOT EXISTS club (
+            club_id INT(11) NOT NULL AUTO_INCREMENT,
+            club VARCHAR(255) NOT NULL,
+            PRIMARY KEY (club_id),
+            UNIQUE KEY (club)
+        )  ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci''')
+        cursor.execute('INSERT IGNORE INTO club (club) VALUES ' + ', '.join(['(%s)'] * len(clubs)), clubs)
+        cursor.execute('SELECT * FROM club')
+        db_clubs = {}
+        for db_club in cursor.fetchall():
+            db_clubs[db_club['club']] = db_club['club_id']
+        cursor.execute('''
+        CREATE TABLE IF NOT EXISTS player (
+            player_id INT(11) NOT NULL AUTO_INCREMENT,
+            name VARCHAR(255) NOT NULL,
+            age TINYINT(4) NOT NULL,
+            external_id INT(11) NOT NULL,
+            wage INT(11) NOT NULL,
+            overall_rate TINYINT(4) NOT NULL,
+            club_id INT(11) DEFAULT NULL,
+            nationality_id INT(11) NOT NULL,
+            position_in_sprite INT(11) NOT NULL,
+            PRIMARY KEY (player_id),
+            UNIQUE KEY (external_id),
+            KEY age (age),
+            CONSTRAINT fk__player_nationality FOREIGN KEY (nationality_id) REFERENCES nationality (nationality_id) ON DELETE CASCADE ON UPDATE CASCADE,
+            CONSTRAINT fk__player_club FOREIGN KEY (club_id) REFERENCES club (club_id) ON DELETE CASCADE ON UPDATE CASCADE
+        )  ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci''')
+        players_data = []
+        for idx, player in players.iterrows():
+            players_data.append(player['Name'])
+            players_data.append(player['Age'])
+            players_data.append(player['ID'])
+            players_data.append(player['Wage'].replace('€', '').replace('K', ''))
+            players_data.append(player['Overall'])
+            if str(player['Club']) != 'nan':
+                players_data.append(db_clubs[player['Club']])
+            else:
+                players_data.append(None)
+            players_data.append(db_nationalities[player['Nationality']])
+            players_data.append(player['position_in_sprite'])
+        cursor.execute('''
+        INSERT IGNORE INTO player (name, age, external_id, wage, overall_rate, club_id, nationality_id, position_in_sprite)
+        VALUES ''' + ', '.join(['(%s, %s, %s, %s, %s, %s, %s, %s)'] * len(players)), players_data)
--- server.py
+++ server.py
@@ -1,14 +1,48 @@
 # uwsgi --socket :9000 --wsgi-file server.py
+import sys
 import pandas as pd
+import pymysql
+import json
 from urllib import parse
+from pymysql.cursors import DictCursor
+from contextlib import closing
+
+
+sys.path.append('/srv/alien')
+
+import config
 
 def application(env, start_response):
     start_response('200 OK', [('Content-Type', 'text/html')])
+    res = []
     query = dict(parse.parse_qsl(env['QUERY_STRING']))
-    players = pd.read_csv('data.csv')
-    players.rename(columns={'Unnamed: 0':''}, inplace=True)
     if 'age' in query:
-        players_filtered = players[players.Age.isin(query['age'].split(','))]
-        return [players_filtered.to_json(orient='records').encode()]
-    else:
-        return []
+        # players = pd.read_csv('data.csv')
+        # players.rename(columns={'Unnamed: 0':''}, inplace=True)
+        # players_filtered = players[players.Age.isin(query['age'].split(','))]
+        # return [players_filtered.to_json(orient='records').encode()]
+
+        ages = query['age'].split(',')
+        with closing(pymysql.connect(**config.MYSQL)) as connection:
+            with connection.cursor(DictCursor) as cursor:
+                cursor.execute('''
+                SELECT
+                    wage AS Wage,
+                    position_in_sprite AS Pos,
+                    age as Age,
+                    overall_rate as Overall,
+                    external_id as ID
+                FROM player
+                LEFT JOIN nationality USING(nationality_id)
+                LEFT JOIN club USING (club_id)
+                WHERE age IN(''' + ', '.join(['%s'] * len(ages)) + ''')
+                ORDER BY Wage ASC''', ages)
+                res = cursor.fetchall()
+                ''',
+                    name as Name,
+                    club as Club,
+                    nationality as Nationality'''
+
+        res.append(json.dumps(res).encode())
+
+    return res
